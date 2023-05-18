0dc579412aa6d3a62e8d0b9912bc067a3144d9ac
Eduardo Nunes <eduardonunesbr@gmail.com>
2023-05-18 08:58:54 -0300

    1.1
    atualizado o modelo dos botoes e o grid dos produtos

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log('teste')
+//filtro portfolio
+$('.filter-btn').on('click', function(){
+    let type = $(this).attr('id');
+    let boxes = $('.project-box');
+    
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+
+});

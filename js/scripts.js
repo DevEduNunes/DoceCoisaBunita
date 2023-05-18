7cc3069cb3991496834d96a288ec26e5de01d029
Eduardo Nunes <eduardonunesbr@gmail.com>
2023-05-18 16:48:01 -0300

    1.2
    java script atualizado porem n finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 //filtro portfolio
 $('.filter-btn').on('click', function(){
+
     let type = $(this).attr('id');
     let boxes = $('.project-box');
     
     $('.main-btn').removeClass('active');
     $(this).addClass('active');
 
+    if(type == 'bolosdec-btn') {
+        eachBoxes('bolosdec')
+    }
 });
+
+function eachBoxes(type, boxes) {
+
+    if(type == 'all') {
+        $(boxes).fadeIn();
+    } else {
+        $(boxes).each(function() {
+            if(!$(this).hasClass(type)) {
+                $(this).fadeOut('slow');
+            } else {
+                $(this).fadeIn();
+            }
+        });
+    }
+}
